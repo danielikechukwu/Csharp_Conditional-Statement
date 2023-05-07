2ba0b3c5f1977bc9776ed1c877173baed38919f4
Daniel ikechukwu <danielikechukwu495@gmail.com>
2023-05-07 13:50:00 +0100

    Add project files.

---
+++ ConditionStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int John = 280;
+
+            int Peter = 24;
+
+            if (John > Peter)
+            {
+                Console.WriteLine("John is older than Peter");
+            }
+
+            else
+            {
+                Console.WriteLine("Peter is older than John");
+            }
+
+        }
+    }
+}
